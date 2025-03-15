43435656476da1df88e99e18396fb1036d261f87
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-15 20:09:23 +0530

    token schema and validation methods created

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -1,4 +1,41 @@
+const logger = require('../utils/logger')
+const {validateRegistration} = require('../utils/validation')
+const User = require("../models/user")
+
+
 // user registration
+
+const registerUser = async(req,res)=>{
+    logger.info('Registration endpoint hit...')
+    try {
+
+        // validate schema
+        const {error} = validateRegistration(req.body)
+        if(error){
+            logger.warn('Validation error',error.details[0].message)
+            return res.status(400).json({
+                success:false,
+                message:error.details[0].message
+            })
+        }
+
+        const {email,username,password} = req.body
+
+        let user = await User.findOne({$or:[{email},{username}]})
+        if(user){
+            logger.warn('User already exist')
+            return res.status(400).json({
+                success:false,
+                message:"User already exists"
+            })
+        }
+
+        user = new User({username,email,password})
+        await user.save()
+    } catch (e) {
+        logger.warn("User saved successfully",user._id);
+    }
+}
 
 
 // user login
