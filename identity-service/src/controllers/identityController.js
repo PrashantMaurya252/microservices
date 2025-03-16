9fd7e12fc4e39cd346ff0437e9cbb817579267a8
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-16 20:24:09 +0530

    refresh token controller created

--- identity-service/src/controllers/identityController.js
+++ identity-service/src/controllers/identityController.js
@@ -2,6 +2,7 @@ const logger = require('../utils/logger')
 const {validateRegistration, validateLogin} = require('../utils/validation')
 const User = require("../models/user")
 const generateToken = require('../utils/generateTokens')
+const RefreshToken = require('../models/RefreshToken')
 
 
 // user registration
@@ -107,8 +108,61 @@ const loginUser = async(req,res)=>{
 
 // refresh token
 
+const refreshTokenUser = async(req,res)=>{
+    logger.info("Refresh token endpoint hit...")
+    try {
+        const {refreshToken} = req.body
+        if(!refreshToken){
+            logger.warn("Refresh Token missing")
+            return res.status(400).json({
+                success:false,
+                message:"Refresh Token Missing"
+            })
+        }
+
+        const storedToken = await RefreshToken.findOne({token:refreshToken})
+
+        if(!storedToken || storedToken.expiresAt < new Date()){
+            logger.warn('Invalid or expired refresh token')
+
+            return res.status(401).json({
+                success:false,
+                message:'Invalid or expired refresh token'
+            })
+        }
+
+        const user = await User.findById(storedToken.user)
+
+        if(!user){
+            logger.warn('User not found')
+
+            return res.status(401).json({
+                success:false,
+                message:'User not found'
+            })
+        }
+
+        const {accessToken:newAccessToken,refreshToken:newRefreshToken} = await generateToken(user)
+
+        // delete the old refreshToken
+
+        await RefreshToken.deleteOne({_id:storedToken._id})
+
+        res.json({
+            accessToken:newAccessToken,
+            refreshToken:newRefreshToken
+        })
+    } catch (e) {
+        logger.error("refresh token error occured",e)
+        res.status(500).json({
+            success:false,
+            message:"Internal Server Error"
+        }) 
+    }
+}
+
 
 // logout
 
 
-module.exports = {registerUser,loginUser}
+module.exports = {registerUser,loginUser,refreshTokenUser}
