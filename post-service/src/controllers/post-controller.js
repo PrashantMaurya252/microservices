7adb422ad08121c969f0d7bd76528682e962d9e9
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-29 23:44:27 +0530

    create post

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -1,8 +1,19 @@
 const Post = require('../models/Post')
 const logger = require('../utils/logger')
+const { validateCreatePost } = require('../utils/validation')
 
 const createPost = async(req,res)=>{
+    logger.info("Create Post endpoint hit")
     try {
+
+        const {error} = validateCreatePost(req.body)
+                if(error){
+                    logger.warn('Validation error',error.details[0].message)
+                    return res.status(400).json({
+                        success:false,
+                        message:error.details[0].message
+                    })
+                }
         const {content,mediaIds} = req.body
         const newelyCretedPost = new Post({
             user:req.user.userId,
